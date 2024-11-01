9fe91ec186655777a5ca85e9aee9e26cbcc02e40
Leonardo Rafael Guanuco <leomusicocompositor@gmail.com>
2024-11-01 15:52:33 -0300

    acomodando el footer y el header

---
+++ proyectofinal_grupo_1/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer(){
+    return(
+        <>
+            {/* Footer */}
+			<footer className="sticky-footer bg-white">
+				<div className="container my-auto">
+					<div className="copyright text-center my-auto">
+						<span>Copyright &copy; Grupo 1</span>
+					</div>
+				</div>
+			</footer>
+			{/* End of Footer */}
+        </>
+    )
+}
+
+export default Footer
