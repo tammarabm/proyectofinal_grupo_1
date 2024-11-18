3b00345fe181ee490a0b38f6bc51d44c1a4639fb
AngeloCarla <165035713+AngeloCarla@users.noreply.github.com>
2024-11-18 12:31:38 -0300

    Correccion DesafioMatematico
    Se agregan opciones a todos los niveles(Basico, Intermedio, Avanzado) -AngeloCarla

--- proyectofinal_grupo_1/src/components/DesafioMatematico/DesafioMatematico.jsx
+++ proyectofinal_grupo_1/src/components/DesafioMatematico/DesafioMatematico.jsx
@@ -1,62 +1,66 @@
-import { Button, InputGroup, Form, Card, OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { useState } from 'react';
-
+import { Button, Card } from 'react-bootstrap';
+import '../DesafioMatematico/styles/DesafioMatematico.css'
 
 const DesafioMatematico = ({ challenge, verifyAnswer, volverMenu }) => {
-  const [userAnswer, setUserAnswer] = useState(''); // Respuesta del usuario
 
-  // Maneja la verificacion de la respuesta
-  const handleSubmit = () => {
-    verifyAnswer(userAnswer);
-    setUserAnswer('');
+  // Cambio de color para las operaciones
+  const generateRandomColor = () => {
+    const r = Math.floor(Math.random() * 100 + 127);
+    const g = Math.floor(Math.random() * 100 + 127);
+    const b = Math.floor(Math.random() * 100 + 127);
+    return `rgb(${r}, ${g}, ${b})`; // rgb(x, x, x);
   };
+
+  const randomColor = generateRandomColor(); // Para que sean los tres botones del mismo color 
 
   return (
     <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
       <Card className="text-center" style={{ width: '25rem' }}>
         <Card.Header as='h6'>Desafio Matematico</Card.Header>
         <Card.Body>
-          {/* Muestra el desafío matemático dependiendo del tipo de operación */}
-          Resuelve: {challenge.tipoOperacion === 1 ? (
-            <span>
-              {`${challenge.num1[0]}/${challenge.num1[1]} ${challenge.operator} ${challenge.num2[0]}/${challenge.num2[1]}`} {/* Fraccion */}
-            </span>
-          ) : challenge.tipoOperacion === 2 ? (
-            <span>
-              {`${challenge.num1} ${challenge.operator} x = ${challenge.num2}`} {/* Ecuacion 1 */}
-            </span>
-          ) : challenge.tipoOperacion === 3 ? (
-            <span>
-              {`${challenge.num1}x ${challenge.operator} ${challenge.num2} = ${challenge.num3}`} {/* Ecuacion 2 */}
-            </span>
-          ) : (
-            `${challenge.num1} ${challenge.operator} ${challenge.num2}`
-          )} {/* Otras operaciones */}
-          <br />
-          <OverlayTrigger
-            placement="bottom"
-            overlay={<Tooltip id="input-tooltip">Ingresa tu respuesta</Tooltip>}
-          >
-            <InputGroup className="mb-3">
-              <Form.Control
-                type="text"
-                placeholder="Respuesta"
-                value={userAnswer}
-                onChange={(e) => setUserAnswer(e.target.value)}
-                onKeyDown={(event) => {
-                  if (event.key === ' ') {
-                    event.preventDefault(); // Evita el espacio
-                    setUserAnswer(userAnswer + '/'); // Agrega "/" al presionar espacio (para fracciones)
-                  }
-                }}
-              />
-            </InputGroup>
-          </OverlayTrigger>
-          <br />
-          {/* Boton para verificar la respuesta */}
-          <Button variant="outline-danger" onClick={handleSubmit}>Verificar resultado</Button>
-          {/* Boton para volver al inicio */}
-          <Button variant="success" onClick={volverMenu}>Volver al Menu</Button>
+          <div>
+            {/* Muestra el desafío matemático dependiendo del tipo de operación */}
+            <Card.Title>Resuelve:</Card.Title>
+            <div className="operacion">
+              {challenge.tipoOperacion === 1 ? (
+                <p>
+                  {`${challenge.num1[0]}/${challenge.num1[1]} ${challenge.operator} ${challenge.num2[0]}/${challenge.num2[1]}`} {/* Fraccion */}
+                </p>
+              ) : challenge.tipoOperacion === 2 ? (
+                <p>
+                  {`${challenge.num1} ${challenge.operator} x = ${challenge.num2}`} {/* Ecuacion 1 */}
+                </p>
+              ) : challenge.num3 ? (
+                <p>
+                  {`${challenge.num1}x ${challenge.operator} ${challenge.num2} = ${challenge.num3}`} {/* Ecuacion 2 */}
+                </p>
+              ) : (
+                <p>
+                  {`${challenge.num1} ${challenge.operator} ${challenge.num2}`} {/* Otras operaciones */}
+                </p>
+              )}
+            </div>
+            <div className="opciones">
+              {/* Botones de Opciones */}
+              {challenge.options.map((option, idx) => (
+                <Button
+                  variant="outline-secondary"
+                  key={idx}
+                  style={{
+                    backgroundColor: randomColor,
+                    color: '#000',
+                    border: 'none',
+                  }}
+                  onClick={() => verifyAnswer(option)}
+                >
+                  {option}
+                </Button>
+              ))}
+            </div>
+
+            {/* Boton para volver al inicio */}
+            <Button variant="success" onClick={volverMenu}>Volver al Menu</Button>
+          </div>
         </Card.Body>
       </Card>
     </div>
